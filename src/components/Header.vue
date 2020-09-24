be5779b4edccdb9185492dc4594226a3d45ad7db
Marvin Gay <MarvinJGay@protonmail.com>
2020-09-24 17:45:09 -0400

    create Header; cleanup

---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="header">
+    <h1 class="logo">XYMXN</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Header',
+});
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background: #fff;
+  box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.3);
+  display: flex;
+  z-index: 10;
+
+  .logo {
+    color: #c70000;
+  }
+
+  #nav {
+    padding: 30px;
+
+    a {
+      font-weight: bold;
+      color: black;
+      text-decoration: none;
+
+      &:hover {
+        color: #c70000;
+      }
+
+      &.router-link-exact-active {
+        color: #c70000;
+      }
+    }
+  }
+}
+</style>
