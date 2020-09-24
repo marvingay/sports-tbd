79fbb35833e95f9b4fca894d6e122a4e9fe76fc7
Marvin Gay <MarvinJGay@protonmail.com>
2020-09-24 19:34:28 -0400

    updated Header mockup

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,17 @@
 <template>
   <header class="header">
-    <h1 class="logo">XYMXN</h1>
+    <div id="logo">
+      <h1 class="logo">XYMXN</h1>
+    </div>
     <div id="nav">
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
+      <router-link to="/games">Games</router-link>
+      <router-link to="/picks">Picks</router-link>
+    </div>
+    <div id="userOptions">
+      <button><router-link to="/signup">Join</router-link></button>
+      <router-link to="/login">Login</router-link>
     </div>
   </header>
 </template>
@@ -18,21 +26,39 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .header {
+  align-items: center;
   background: #fff;
   box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.3);
   display: flex;
+  justify-content: space-around;
+  position: sticky;
+  text-transform: uppercase;
+  top: 0;
   z-index: 10;
 
-  .logo {
-    color: #c70000;
+  #logo {
+    display: flex;
+    flex: 1;
+    .logo {
+      color: #c70000;
+      font-size: 2em;
+      letter-spacing: 0.2em;
+      margin: 0 auto;
+    }
   }
 
   #nav {
+    justify-content: center;
+    display: flex;
+    flex: 3;
     padding: 30px;
 
     a {
-      font-weight: bold;
       color: black;
+      font-size: 1.25rem;
+      font-weight: bold;
+      margin: 0 15px;
+      padding: 10px 0;
       text-decoration: none;
 
       &:hover {
@@ -40,6 +66,23 @@ export default defineComponent({
       }
 
       &.router-link-exact-active {
+        border-bottom: 2px solid #c70000;
+        color: #c70000;
+      }
+    }
+  }
+
+  #userOptions {
+    flex: 1;
+
+    a {
+      color: black;
+      font-size: 1.25rem;
+      margin: 0 15px;
+      padding: 10px 0;
+      text-decoration: none;
+
+      &:hover {
         color: #c70000;
       }
     }
